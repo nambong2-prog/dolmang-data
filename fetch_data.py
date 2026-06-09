b0984038693d7963285c11baa21222dd8defcba4
nambong2-prog <nambong2@gmail.com>
2026-06-09 20:53:16 +0900

    fix: 제주산 필터 plor_nm→plor_cd="14"로 수정, 디버그 제거
    Refactor condition for hobak data collection to check for Jeju origin code.

--- fetch_data.py
+++ fetch_data.py
@@ -13,7 +13,7 @@ BASE_URL = "https://apis.data.go.kr/B552845/katRealTime2/trades2"
 MANGAM_VARIETIES = ["레드향", "천혜향", "한라봉", "카라향"]
 HOBAK_ITEMS = ["호박"]  # 품목명
 HOBAK_VARIETIES = ["단호박", "미니밤호박"]  # 품종명
-JEJU_ORIGINS = ["제주"]  # 원산지 필드에 "제주" 포함이면 매칭
+JEJU_PLOR_CD = "14"  # 제주 출하지 코드
 TARGET_LCLSF = ["06", "09"]  # 06=과실류(만감류/감귤), 09=과채류(호박)
 
 # 누적 데이터 보관 일수
@@ -96,11 +96,8 @@ def filter_items(all_items):
             mangam_data.append(parse_item(item, "만감류", "품종"))
         elif mclsf_cd != "15" and "감귤" in item_nm:
             gamgyul_data.append(parse_item(item, "감귤", "품종"))
-        elif item.get("gds_lclsf_cd","") == "09" and vrty_nm in HOBAK_VARIETIES:
-            # 디버그: plor_nm 실제값 출력
-            print(f"단호박발견: vrty={vrty_nm}, plor_nm=[{plor_nm}], plor_cd={item.get('plor_cd','')}")
-            if any(j in plor_nm for j in JEJU_ORIGINS):
-                hobak_data.append(parse_item(item, "호박", "품종"))
+        elif item.get("gds_lclsf_cd","") == "09" and vrty_nm in HOBAK_VARIETIES and item.get("plor_cd","") == JEJU_PLOR_CD:
+            hobak_data.append(parse_item(item, "호박", "품종"))
     return mangam_data, gamgyul_data, hobak_data
 
 
